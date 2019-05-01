37bcc7bb2e63b4f6693a1bf78337ce90751e8888
Omar Mohamed <omar.mohamed.latif@gmail.com>
2019-05-01 18:11:36 +0200

    -added birds dataset preprocessing

--- data_loader.py
+++ data_loader.py
@@ -47,27 +47,31 @@ def save_caption_vectors_ms_coco(data_dir, split, batch_size):
 		batch_no += 1
 
 
-def save_caption_vectors_flowers(data_dir):
+def save_caption_vectors_birds(data_dir):
 	import time
-	
-	img_dir = join(data_dir, 'flowers/jpg')
-	image_files = [f for f in os.listdir(img_dir) if 'jpg' in f]
+	img_folders_dir=join(data_dir, 'birds/CUB_200_2011/images')
+	image_folders = [f for f in os.listdir(img_folders_dir)]
+	image_files=[]
+	for image_folder in image_folders:
+		img_dir = join(img_folders_dir, image_folder)
+		image_files.extend([f for f in os.listdir(img_dir) if 'jpg' in f])
 	print(image_files[300:400])
 	print(len(image_files))
 	image_captions = { img_file : [] for img_file in image_files }
 
-	caption_dir = join(data_dir, 'flowers/text_c10')
+	caption_dir = join(data_dir, 'birds/text_c10')
+	class_folders = [f for f in os.listdir(caption_dir)]
+
 	class_dirs = []
-	for i in range(1, 103):
-		class_dir_name = 'class_%.5d'%(i)
-		class_dirs.append( join(caption_dir, class_dir_name))
+	for f in class_folders:
+		class_dirs.append( join(caption_dir, f))
 
 	for class_dir in class_dirs:
 		caption_files = [f for f in os.listdir(class_dir) if 'txt' in f]
 		for cap_file in caption_files:
 			with open(join(class_dir,cap_file)) as f:
 				captions = f.read().split('\n')
-			img_file = cap_file[0:11] + ".jpg"
+			img_file = cap_file[0:-4] + ".jpg"
 			# 5 captions per image
 			image_captions[img_file] += [cap for cap in captions if len(cap) > 0][0:5]
 
@@ -84,11 +88,52 @@ def save_caption_vectors_flowers(data_dir):
 		print("Seconds", time.time() - st)
 		
 	
+	h = h5py.File(join(data_dir, 'birds_tv.hdf5'))
+	for key in encoded_captions:
+		h.create_dataset(key, data=encoded_captions[key])
+	h.close()
+
+
+def save_caption_vectors_flowers(data_dir):
+	import time
+
+	img_dir = join(data_dir, 'flowers/jpg')
+	image_files = [f for f in os.listdir(img_dir) if 'jpg' in f]
+	print(image_files[300:400])
+	print(len(image_files))
+	image_captions = {img_file: [] for img_file in image_files}
+
+	caption_dir = join(data_dir, 'flowers/text_c10')
+	class_dirs = []
+	for i in range(1, 103):
+		class_dir_name = 'class_%.5d' % (i)
+		class_dirs.append(join(caption_dir, class_dir_name))
+
+	for class_dir in class_dirs:
+		caption_files = [f for f in os.listdir(class_dir) if 'txt' in f]
+		for cap_file in caption_files:
+			with open(join(class_dir, cap_file)) as f:
+				captions = f.read().split('\n')
+			img_file = cap_file[0:11] + ".jpg"
+			# 5 captions per image
+			image_captions[img_file] += [cap for cap in captions if len(cap) > 0][0:5]
+
+	print(len(image_captions))
+
+	model = skipthoughts.load_model()
+	encoded_captions = {}
+
+	for i, img in enumerate(image_captions):
+		st = time.time()
+		encoded_captions[img] = skipthoughts.encode(model, image_captions[img])
+		print(i, len(image_captions), img)
+		print("Seconds", time.time() - st)
+
 	h = h5py.File(join(data_dir, 'flower_tv.hdf5'))
 	for key in encoded_captions:
 		h.create_dataset(key, data=encoded_captions[key])
 	h.close()
-			
+
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument('--split', type=str, default='train',
@@ -97,12 +142,17 @@ def main():
                        help='Data directory')
 	parser.add_argument('--batch_size', type=int, default=64,
                        help='Batch Size')
-	parser.add_argument('--data_set', type=str, default='flowers',
+	parser.add_argument('--data_set', type=str, default='flowers&birds',
                        help='Data Set : Flowers, MS-COCO')
 	args = parser.parse_args()
-	
+
 	if args.data_set == 'flowers':
 		save_caption_vectors_flowers(args.data_dir)
+	elif args.data_set == 'birds':
+		save_caption_vectors_birds(args.data_dir)
+	elif args.data_set == 'flowers&birds':
+		save_caption_vectors_flowers(args.data_dir)
+		save_caption_vectors_birds(args.data_dir)
 	else:
 		save_caption_vectors_ms_coco(args.data_dir, args.split, args.batch_size)
 
